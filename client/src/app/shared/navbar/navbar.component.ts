06f0bae739d68358223c68792d2e9b8bead17b95
marco <you@example.com>
2022-11-24 13:19:44 -0500

    shared modules added

---
+++ client/src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  get cart() {
+    return this.shoppingcartService.cart
+  }
+
+  constructor(
+    private shoppingcartService: ShoppingCartService,
+    private route:Router) { }
+
+  isInCheckoutPath(){
+    return this.route.url === "/checkout"
+  }
+  
+  ngOnInit(): void {
+  }
+
+}
