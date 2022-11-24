f8cf4bb3883565a7bec629dddf112a58045e9558
marco <you@example.com>
2022-11-24 15:38:02 -0500

    paginate in products

--- api/controllers/product.controllers.js
+++ api/controllers/product.controllers.js
@@ -2,9 +2,28 @@
 const { request, response } = require("express")
 const Product = require("../models/Product")
 const getProducts = async (req = request, res = response) => {
+    const { page: pagequery = 1 } = req.query;
+    const page = Number(pagequery)
+    const limit = 2
+    const skip = (Number(page) - 1) * limit
     try {
+
+        const allEntries = await Product.count();
+        if (allEntries < skip) { return res.status(400).json({ ok: false, message: `doesnt exist any element in page ${page}` }) }
         const data = await Product.find()
-        res.status(200).json({ ok: true, data, prevPage: null, nextPage: null })
+            .limit(limit)
+            .skip(skip)
+
+        const totalPages = Math.ceil(allEntries / Number(limit))
+        res.status(200).json({
+            ok: true,
+            allEntries,
+            totalPages,
+            data,
+            currentPage: page,
+            prevPage: page > 1 ? page - 1 : null,
+            nextPage: page < totalPages ? page + 1 : null
+        });
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
--- api/routes/products.routes.js
+++ api/routes/products.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require("express")
 const { getProducts, postProduct } = require("../controllers/product.controllers")
 const productRouter = Router()
+const {param} =require("express-validator")
 
-productRouter.get("/", getProducts)
+
+productRouter.get("/",[
+    param("page").isNumeric().withMessage("must be a valid value"),
+],getProducts)
 productRouter.post("/", postProduct)
 
 
